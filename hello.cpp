96d9be2fd69e70b477e79fb9b43ad62fe239f970
Sanchit Dhiman <sanchitdhiman04@gmail.com>
2025-02-22 17:05:29 +0530

    Added multi-line comment

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+/* This program prints
+   "Hello, C++ Beginner!"
+   to the console */
+
 int main() {    // Declaring Main function
     std::cout << "Hello, C++ Beginner!" << std::endl;   // Printing to the console
 
